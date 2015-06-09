0268707696e3ab9f00d40ae4b0e302411742c5e5
sublink <nicolas@biodigitals.com>
2015-06-09 17:03:22 +0200

    updated revision

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import codecs
 
 setup(
     name='Apetizer',
-    version='0.3.0',
+    version='0.3.1',
     author='Nicolas Danjean',
     author_email='nicolas@biodigitals.com',
     packages=['apetizer',
